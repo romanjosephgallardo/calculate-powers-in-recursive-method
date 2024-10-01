6d0e2f298fdaca550c8509dcff2f72f26a5e9d9b
Roman Joseph Gallardo <romanjosephgallardo01@gmail.com>
2024-10-01 15:34:21 +0800

    Remove unwanted docstrings and comments

--- calculate_powers.py
+++ calculate_powers.py
@@ -1,18 +1,4 @@
-'''
-A teacher named Mrs. Rivera who loved making math fun for her students. 
-One day, a student named Mia asked how to calculate powers, like (2^7). 
-Mrs. Rivera explained, “Imagine you need to multiply 2 by itself seven times. 
-We can solve this using a python code utilizing  special method called recursion 
-to solve this,” Can you help the class of Mrs. Rivera by providing the recursive 
-method to calculate powers?
-'''
-
-# pseudocode
-
-# code here
-# Get user input of base and power
-entered_base = int(input("Enter the base number: "))
-entered_power = int(input("Enter the power: "))
+# Calculate powers in recursive method:
 
 # Define calculate powers function
 def calculate_powers(base, power):
@@ -21,7 +7,12 @@ def calculate_powers(base, power):
     else:
         return 1
 
+# Get the user input of base and power
+entered_base = int(input("Enter the base number: "))
+entered_power = int(input("Enter the power: "))
+
 # Call the function
 result = calculate_powers(entered_base, entered_power)
+
 # Print the result
 print(f"{entered_base}^{entered_power} = {result}")
