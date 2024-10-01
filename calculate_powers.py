fddbb7013d827ffde193705b385ed383c57a206f
Roman Joseph Gallardo <romanjosephgallardo01@gmail.com>
2024-10-01 15:30:33 +0800

    Add power computation and result output.

--- calculate_powers.py
+++ calculate_powers.py
@@ -11,9 +11,17 @@ method to calculate powers?
 
 # code here
 # Get user input of base and power
-base = int(input("Enter the base number:"))
-power = int(input("Enter the power: "))
+entered_base = int(input("Enter the base number: "))
+entered_power = int(input("Enter the power: "))
 
 # Define calculate powers function
+def calculate_powers(base, power):
+    if power != 0:
+        return base * calculate_powers(base, power - 1)
+    else:
+        return 1
+
 # Call the function
+result = calculate_powers(entered_base, entered_power)
 # Print the result
+print(f"{entered_base}^{entered_power} = {result}")
