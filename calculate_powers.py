ccce0a1bc252297a607f5c82b50670260e0fb3f0
Roman Joseph Gallardo <romanjosephgallardo01@gmail.com>
2024-10-01 13:58:19 +0800

    Add pseudocode

---
+++ calculate_powers.py
@@ -0,0 +1,16 @@
+'''
+A teacher named Mrs. Rivera who loved making math fun for her students. 
+One day, a student named Mia asked how to calculate powers, like (2^7). 
+Mrs. Rivera explained, “Imagine you need to multiply 2 by itself seven times. 
+We can solve this using a python code utilizing  special method called recursion 
+to solve this,” Can you help the class of Mrs. Rivera by providing the recursive 
+method to calculate powers?
+'''
+
+# pseudocode
+
+# code here
+# Get user input of base and power
+# Define calculate powers function
+# Call the function
+# Print the result
